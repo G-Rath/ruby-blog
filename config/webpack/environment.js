8d9ac370a25e5e8e3fbd32220de644b83f49c812
Gareth Jones <jones258@gmail.com>
2019-01-10 10:05:43 +1300

    Add `awesome-typescript-loader` to `webpack` env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker');
 
+/** @type {ConfigList} */
+const environmentLoaders = environment.loaders;
+
+// add the typescript loader
+environmentLoaders.append(
+  'typescript',
+  {
+    test: /\.tsx?$/,
+    loader: 'awesome-typescript-loader',
+    options: {
+      configFileName: './tsconfig.json'
+    }
+  }
+);
+
 module.exports = environment;
