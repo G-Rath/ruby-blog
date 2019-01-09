81027b667e45d4db023c126ee5111cd1c4c01ad7
Gareth Jones <jones258@gmail.com>
2019-01-10 10:37:16 +1300

    Add `jQuery` to `application.ts`

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -7,6 +7,19 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-const myFn = (param: string) => console.log(param);
+import jQuery from 'jquery';
 
-myFn('Hello World from Webpacker');
+/*
+  jQuery definitions declare it as global, and it usually is,
+  so for the sake of ease-of-development, just assign it to the window.
+ */
+// @ts-ignore -- technically an error b/c window.$ isn't defined
+window.$ = jQuery;
+
+$(() => {
+  const myFn = (param: string) => console.log(param);
+
+  myFn('Hello World from Webpacker');
+
+  console.log($('body'));
+});
