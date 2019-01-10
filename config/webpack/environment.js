fa7c5cd0d71e09f165bbcca483cc3141853a679d
Gareth Jones <jones258@gmail.com>
2019-01-10 13:30:12 +1300

    Add path aliases for `src`, `packs`, & `styles`

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,14 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
+const path = require('path');
+
+/**
+ * Constant string-path that leads to the root of the project.
+ *
+ * @type {string}
+ * @constant
+ */
+const PATH_TO_ROOT = path.resolve(__dirname, '..', '..');
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
@@ -27,6 +36,13 @@ environmentLoaders.append(
 
 // ensure we don't emit on error
 environment.config.merge({
+  resolve: {
+    alias: {
+      '@src': path.resolve(PATH_TO_ROOT, 'app/webpacker/src'),
+      '@pack': path.resolve(PATH_TO_ROOT, 'app/webpacker/pack'),
+      '@styles': path.resolve(PATH_TO_ROOT, 'app/webpacker/styles')
+    }
+  },
   optimization: {
     noEmitOnErrors: true
   }
