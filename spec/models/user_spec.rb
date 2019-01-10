f81372d4904ec87362912c0540fca9dea98536fd
Gareth Jones <jones258@gmail.com>
2019-01-11 10:22:41 +1300

    Create basic `user` model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.build(:user) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  describe "is not valid when" do
+    it "email is missing" do
+      subject.email = nil
+
+      expect(subject).to_not be_valid
+    end
+
+    it "email is not an email" do
+      subject.email = "not an email"
+
+      expect(subject).to_not be_valid
+    end
+
+    it "first_name is missing" do
+      subject.first_name = nil
+
+      expect(subject).to_not be_valid
+    end
+
+    it "last_name is missing" do
+      subject.last_name = nil
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
