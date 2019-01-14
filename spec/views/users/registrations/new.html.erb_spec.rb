4b8c97a512b731751f059ecabddf0759f9371134
Gareth Jones <jones258@gmail.com>
2019-01-15 12:04:45 +1300

    Adjust `new.html.erb_spec` tests to use symbols instead of strings

--- spec/views/users/registrations/new.html.erb_spec.rb
+++ spec/views/users/registrations/new.html.erb_spec.rb
@@ -8,23 +8,23 @@ RSpec.describe "users/registrations/new.html.erb", type: :view do
   end
 
   it "should contain an email input for 'email'" do
-    expect(rendered).to have_field("user[email]", type: "email")
+    expect(rendered).to have_field(:user_email, type: "email")
   end
 
   it "should contain a text input for 'first_name'" do
-    expect(rendered).to have_field("user[first_name]", type: "text")
+    expect(rendered).to have_field(:user_first_name, type: "text")
   end
 
   it "should contain a text input for 'last_name'" do
-    expect(rendered).to have_field("user[last_name]", type: "text")
+    expect(rendered).to have_field(:user_last_name, type: "text")
   end
 
   it "should contain a password input for 'password'" do
-    expect(rendered).to have_field("user[password]", type: "password")
+    expect(rendered).to have_field(:user_password, type: "password")
   end
 
   it "should contain a password input for 'password_confirmation'" do
-    expect(rendered).to have_field("user[password_confirmation]", type: "password")
+    expect(rendered).to have_field(:user_password_confirmation, type: "password")
   end
 
   it "should contain a submit button labeled 'Sign Up'" do
